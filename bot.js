c16323f430a7c25806aff12f36fc3fa9c5796bbc
JuliePreperier <JuliePreperier@users.noreply.github.com>
2018-07-04 17:54:53 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,9 +9,6 @@ client.on('message', message => {
     if (message.content === '!planning') {
     	message.reply('Voici le planning de cette semaine: ', {files: ["http://www.versustv.ch/wp-content/uploads/2018/07/planning27.png"]});
   	}
-    else if(message.content === '!cmd'){
-        message.replay('les commandes possible sont:\n - !planning : pour connaitre le planning de la semaine\n - !rs : pour connaitre tous les réseaux sociaux de l''asscociation Versus\n - !cmd : Pour afficher le guide des commandes disponible sur le serveur Versus');
-    }
 });
 
 client.on('guildMemberAdd', member => {
