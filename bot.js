cd2529be3eb76896661738f7edb63f566a5bbd4e
JuliePreperier <JuliePreperier@users.noreply.github.com>
2018-07-04 18:00:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,6 +9,13 @@ client.on('message', message => {
     if (message.content === '!planning') {
     	message.reply('Voici le planning de cette semaine: ', {files: ["http://www.versustv.ch/wp-content/uploads/2018/07/planning27.png"]});
   	}
+    if (message.content === '!rs'){
+        message.reply('Notre page Facebook : https://www.facebook.com/CHVersusTV/');
+        message.reply('Notre page Twitter : https://www.twitter.com/versustv1');
+        message.reply('Notre page Instagram : https://www.instagram.com/versus.tv/');
+        message.reply('Notre page Twitch : https://www.twitch.tv/tvversus');
+        message.reply('Notre page web : https://www.versustv.ch/');
+    }
 });
 
 client.on('guildMemberAdd', member => {
