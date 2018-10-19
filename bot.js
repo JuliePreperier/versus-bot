e09633c468fc7e092d1e829577f059ec57cdfc25
JuliePreperier <JuliePreperier@users.noreply.github.com>
2018-10-19 09:57:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,6 +6,21 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
+client.on('message', message => {
+    if (message.content === 'ping'){
+        message.reply('pong');
+    }
+    if (message.content === '!planning') {
+    	message.reply('Voici le planning de cette semaine : ', {files: ["http://www.versustv.ch/wp-content/uploads/2018/08/planning33.png"]});
+  	}
+    if (message.content === '!rs'){
+        message.reply('Notre page Facebook : https://www.facebook.com/CHVersusTV/');
+        message.reply('Notre page Twitter : https://www.twitter.com/versustv1');
+        message.reply('Notre page Instagram : https://www.instagram.com/versus.tv/');
+        message.reply('Notre page Twitch : https://www.twitch.tv/tvversus');
+    }
+});
+
 client.on('guildMemberAdd', member => {
     var role = member.guild.roles.find('name', 'Les Gens');
    
